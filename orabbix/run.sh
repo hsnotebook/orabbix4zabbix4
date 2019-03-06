54265907b3c078bfb41b3cfae56d8376c69250f3
huangshuai <huangshuai010@gmail.com>
2019-03-06 16:49:36 +0800

    Add config.props and modify run.sh for docker.

--- orabbix/run.sh
+++ orabbix/run.sh
@@ -1,1 +1,18 @@
-java -Duser.language=en -Duser.country=US -Dlog4j.configuration=./conf/log4j.properties -cp $(for i in lib/*.jar ; do echo -n $i: ; done).:./orabbix-1.2.3.jar com.smartmarmot.orabbix.bootstrap start ./conf/config.props &
+#!/bin/bash
+
+CONFIG_FILE="/opt/orabbix/conf/config.props"
+
+sed -i "s/ZBX_SERVER_HOST/${ZBX_SERVER_HOST:-127.0.0.1}/g" ${CONFIG_FILE}
+sed -i "s/ZBX_SERVER_PORT/${ZBX_SERVER_PORT:-10051}/g" ${CONFIG_FILE}
+sed -i "s/ORACLE_HOST/${ORACLE_HOST:-127.0.0.1}/g" ${CONFIG_FILE}
+sed -i "s/ORACLE_PORT/${ORACLE_PORT:-1521}/g" ${CONFIG_FILE}
+sed -i "s/ORACLE_USER/${ORACLE_USER:-zabbix}/g" ${CONFIG_FILE}
+sed -i "s/ORACLE_PASSWORD/${ORACLE_PASSWORD:-zabbix}/g" ${CONFIG_FILE}
+sed -i "s/ORACLE_INSTANCE/${ORACLE_INSTANCE:-orcl}/g" ${CONFIG_FILE}
+
+java \
+	-Duser.timezone=Asia/Shanghai \
+	-Dlog4j.configuration=./conf/log4j.properties \
+	-cp $(for i in lib/*.jar ; do echo -n $i: ; done).:./orabbix-1.2.3.jar \
+	com.smartmarmot.orabbix.bootstrap \
+	start ./conf/config.props
